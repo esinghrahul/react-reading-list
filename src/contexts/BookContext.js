60d5438a5acd0d80d41dfb0286782dca005d865f
esinghrahul <e.singh.rahul@outlook.com>
2020-06-21 13:54:51 +0530

    Added and incorporated book reducers to avoid passing individual methds as props to components

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,13 +1,17 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useReducer } from 'react'
 import {v4 as uuid} from 'uuid'
+import { bookReducer } from '../reducers/book-reducer'
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
+    //useState Hooks for setting initial values
+    /*
     const [books, setBooks] = useState([
     {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
     {title: 'the final empire', author: 'brandon sanderson', id: 2},
     ])
+    
 
     const addBook = (title, author) => {
         setBooks([...books, {title: title, author: author, id: uuid() }])
@@ -18,6 +22,15 @@ const BookContextProvider = (props) => {
     }
     return(
         <BookContext.Provider value = {{books, addBook, removeBook}}>
+            {props.children}
+        </BookContext.Provider>
+    )
+    */
+
+    //useReducer hook to define initial book State and send a dispatch function rather than passing all relevant functions individually
+    const [books, dispatch] = useReducer(bookReducer, [])
+    return(
+        <BookContext.Provider value = {{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
