5d0309bf52bde6e3db208f310eab7baffee62426
esinghrahul <e.singh.rahul@outlook.com>
2020-07-02 21:24:11 +0530

    Updates

---
+++ precache-manifest.93b8464d71dd8dcd814d913887f365f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b84e0704b774a07bfb929c85a67cef9",
+    "url": "/react-reading-list/index.html"
+  },
+  {
+    "revision": "fc95cbf5679c1b14f267",
+    "url": "/react-reading-list/static/css/main.2fb071f8.chunk.css"
+  },
+  {
+    "revision": "7e9fa8925cb33ed1008f",
+    "url": "/react-reading-list/static/js/2.f6444488.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-reading-list/static/js/2.f6444488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc95cbf5679c1b14f267",
+    "url": "/react-reading-list/static/js/main.69580289.chunk.js"
+  },
+  {
+    "revision": "f8ebc388aa8e21ce88aa",
+    "url": "/react-reading-list/static/js/runtime-main.38e45a2e.js"
+  }
+]);
